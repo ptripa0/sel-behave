239fc944855c13ab040ab1022316a2dc7b0e470d
ptripa0 <prabodhtripathi@gmail.com>
2021-09-11 14:56:14 +0530

    5th Commit from Pycharm

--- features/steps/login.py
+++ features/steps/login.py
@@ -1,15 +1,22 @@
 from behave import *
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @given(u'I launch the chrome browser')
-def step_impl(context):
+def launch_browser(context):
     print("In Given")
+    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    context.driver.get("https://opensource-demo.orangehrmlive.com/")
     assert True
 
 
 @then(u'Enter username and password')
 def step_impl(context):
-    assert context.failed is False
+    logo = context.driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/img[1]").is_displayed()
+    assert logo is True
+    #context.driver.find_element_by_xpath("//input[@id='txtUsername']").is_displayed()
+
 
 
 @then(u'Click on login button')
